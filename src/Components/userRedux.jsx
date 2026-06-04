1d7ec1804d8713ca641a13214a0799db7bbded97
Animediscussion <prasan1010jeet@gmail.com>
2026-06-04 13:56:05 +0530

    param

--- src/Components/userRedux.jsx
+++ src/Components/userRedux.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUserMiddleware } from "./middleware/userMiddleware";
 
 const userRedux = () => {
   const dispatch = useDispatch();
+  useDispatch();
   const { loading, error, user, param } = useSelector(
     (store) => store.userState,
   );
+
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (param) {
+      dispatch(fetchUserMiddleware(param));
+    }
+  }, [param, dispatch]);
+
+  const heading = <h2>User Example</h2>;
   if (loading) {
     return (
       <>
